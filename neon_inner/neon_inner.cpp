953d9219873c4ac0181703fbd2350c8ea285b86b
shiquanyu <crashdown@foxmail.com>
2021-03-15 11:57:14 +0800

    update, speed up 50%

--- neon_inner/neon_inner.cpp
+++ neon_inner/neon_inner.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <arm_neon.h>
-#include <arm_fp16.h>
 #include <sys/time.h>
 
 using namespace std;
@@ -24,19 +23,17 @@ float nn_inner_neon(float *buf1, float *buf2, const size_t len)
 {
     float inner_product = 0.0f;
     float tmp[4];
-    float32x4_t fsum = vdupq_n_f32(0);
+    float32x4_t fsum = vdupq_n_f32(0.f);
+    float32x4_t fmul;
     size_t count = len / 4;
     size_t rest = len % 4;
 
     if (!buf1 || !buf2 || len == 0)
         return 0.f;
 
-    while (count--)
+    for (size_t i = 0; i < count; ++i)
     {
-        float32x4_t fbuf1 = vld1q_f32(buf1);
-        float32x4_t fbuf2 = vld1q_f32(buf2);
-        float32x4_t fmul = vmulq_f32(fbuf1, fbuf2);
-        fsum = vaddq_f32(fsum, fmul);
+        fsum = vmlaq_f32(fsum, vld1q_f32(buf1), vld1q_f32(buf2));
         buf1 += 4;
         buf2 += 4;
     }
@@ -47,9 +44,7 @@ float nn_inner_neon(float *buf1, float *buf2, const size_t len)
     }
     if (rest)
     {
-        float32x4_t fbuf1 = vld1q_f32(buf1);
-        float32x4_t fbuf2 = vld1q_f32(buf2);
-        float32x4_t fmul = vmulq_f32(fbuf1, fbuf2);
+        fmul = vmulq_f32(vld1q_f32(buf1), vld1q_f32(buf2));
         vst1q_f32(tmp, fmul);
         for (size_t i = 0; i < rest; ++i)
         {
@@ -67,21 +62,22 @@ int main(int argc, char const *argv[])
     float feat2[FEAT_LEN] = {0};
     for (size_t i = 0; i < FEAT_LEN; i++)
     {
-        feat1[i] = rand() / double(RAND_MAX);
-        feat2[i] = rand() / double(RAND_MAX);
+        feat1[i] = rand() / float(RAND_MAX);
+        feat2[i] = rand() / float(RAND_MAX);
     }
 
     size_t loop_time = 100000;
+    int elasped_time;
     struct timeval tv_start;
     struct timeval tv_end;
-    printf("loop %zu times.\n", loop_time);
+    printf("loop %zu times with %d float.\n", loop_time, FEAT_LEN);
     gettimeofday(&tv_start, NULL);
     for (size_t i = 0; i < loop_time; ++i)
     {
         nn_inner(feat1, feat2, FEAT_LEN);
     }
     gettimeofday(&tv_end, NULL);
-    int elasped_time = (tv_end.tv_sec - tv_start.tv_sec) * 1000 + (tv_end.tv_usec - tv_start.tv_usec) / 1000;
+    elasped_time = (tv_end.tv_sec - tv_start.tv_sec) * 1000 + (tv_end.tv_usec - tv_start.tv_usec) / 1000;
     printf("time normal: %dms\n", elasped_time);
 
     gettimeofday(&tv_start, NULL);
@@ -96,6 +92,9 @@ int main(int argc, char const *argv[])
     printf("inner normal: %f\n", nn_inner(feat1, feat2, FEAT_LEN));
     printf("inner neon: %f\n", nn_inner_neon(feat1, feat2, FEAT_LEN));
 
+#ifdef __ARM_NEON
+    printf("__ARM_NEON: %d\n", __ARM_NEON);
+#endif
 
     return 0;
 }
